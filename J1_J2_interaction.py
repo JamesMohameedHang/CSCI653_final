2e1e116f5ddce0c0db0061463562ec1c10d81090
JamesMohameedHang <hangyuxiao@MacBookAir.attlocal.net>
2025-11-29 17:00:25 -0800

    Added readme file

---
+++ J1_J2_interaction.py
@@ -0,0 +1,84 @@
+import numpy as np
+import math
+import tenpy
+from tenpy.networks.mps import MPS
+import time
+from tenpy.models.xxz_chain import XXZChain
+from scipy.optimize import curve_fit
+from tenpy.algorithms import dmrg
+
+deltas = np.arange(-1.0,1.5,0.05)
+#deltas1=np.arange(-1.00,-0.60,0.05)
+#deltas2=np.arange(0.65,1.05,0.05)
+#deltas=np.arange(-1.0,1.05,0.05)
+delta=0.342
+Del=4.0
+Ls=np.arange(50,530,20)
+L = 50
+J = 1.0
+Jz = 0.0
+f=open("edge_mode/ssh_interaction_strong_Jz_4_L_50_Sz_haldane.txt","w")
+for delta in deltas:
+	N=L*2
+	J2 = J+delta
+	model_params = {
+            'L': N,
+            'Jxx': J,
+            'Jz':Jz,
+            'hz':0,  # just free fermions, but you can generalize...
+            'bc_MPS':'finite'
+	   }
+	M = XXZChain(model_params)
+	product_state = ['up', 'down']*(L)
+	psi = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
+	chi_max=int(0.4*N)
+	dmrg_params = {
+            'mixer': None,
+            #  'mixer_params': {'amplitude': 1.e-3, 'decay': 5., 'disable_after': 50},
+            'trunc_params': {
+                'chi_max': 300,
+                #Dimension of the system
+                'svd_min': 1.e-10
+            },
+            'max_E_err': 1.e-9,
+            'combine': True
+        }
+	M.add_coupling(J2 * 0.5, 0, 'Sp', 0, 'Sm', 2, plus_hc=True)
+	M.add_coupling(J2*Jz * 0.5, 0, 'Sz', 0, 'Sz', 2)
+    #chi_list = np.arange(7, 31, 2)
+	s_list = []
+	E_list=[]
+	eng = dmrg.TwoSiteDMRGEngine(psi, M, dmrg_params)
+ # necessary if you for example have a fixed numer of sweeps, if you don't set this you option your simulation stops after initial number of sweeps!
+        ##   DMRG Calculation    ##
+	print("Start IDMRG CALCULATION")
+	E,psi=eng.run()
+	eng.options['mixer'] = None
+	psi.canonical_form()
+
+        ##   Calculating bond entropy and correlation length  ##
+	s=psi.entanglement_entropy()[L-1]
+	s2=psi.entanglement_entropy()[L]
+	print("even")
+	corr_func=psi.correlation_function("Sz","Sz")[0][L]
+         # the bond 0 is between MPS unit cells and hence sensible even for 2D lattices.
+        #xi_list.append(psi.correlation_length())
+        #print(result)
+	expec=psi.expectation_value("Sz")
+	#expec2=psi.expectation_value("Sz")
+	print(
+          #np.mean(psi.expectation_value(M.H_bond)),
+        L,
+        s,
+        s2,
+        E,
+        corr_func,
+        delta,
+          #xi_list[-1],
+        flush=True)
+	tenpy.tools.optimization.optimize(3)  # quite some speedup for small chi
+	#f.write(str(int(L))+"       "+str(expec)+"\n")
+	print("SETTING NEW BOND DIMENSION")
+	# f.write(str(int(L))+"       "+str(expec)+str(delta)+"\n")
+	f.write(str(int(L))+"       "+str(s)+"      "+str(s2)+"      "+str(E_list[-1])+"     "+str(E)+"       "+str(delta)+"\n")
+f.close()
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# CSCI653_final
+# Topological Insulator
+Topological Insulators are a remarkable class of quantum material that bahave as ##insulators in their inside## while ##conducting in their outside##. In this way, we could greatly reduce the dissipation in current transportation. So far, physicists have proposed numerous physical and mathematical models in simulating the topological insulator.
+
+# Next-Nearest-Neighbor Spin Chain
+Among the physical models, Next-Nearest Neighbor Spin Chain is one of the most popular ones. The major reason is that 
+1. It supports relatively rich quantum phases
+2. It possesses robust surface state(the conducting surface), 
+3. It could serve as paradigm of wide range of materials
+
+# Model and Hamiltonian
+For a spin chain, we'd use Spin operators for our study. The Hamiltonian is 
+<img width="240" alt="image" src="images/Hamiltonian.png">
+The schematic represents its interactions is
+
+# Method
+In this project, I'll be most interested in the Entanglement Entropy of the ground state. By probing the Entanglement Entropy, I'd like to construct a phase diagram of the model, and obtain an understanding of the property of each phase.
+## Entanglement Entropy
+I bet you guys have heard of Entanglement in Quantum Mechanis. There's a popular story of "spooky action at a distance": having two gloves - you send one to North Pole without looking, and when your friend at South Pole opens theirs and finds a left-handed glove, yours instantly becomes right-handed. This story demonstrates two particles, after interacting, even though being separated, would still affect each other
+
+In modern physics, we have an important variable, called Entanglement Entropy, that could be used to describe how two parts of a system Entangles. This variable is super important since it carries out the property of the whole system and could be served to distinguish two different topological phases.
+
+# Algorithm: Density Matrix Renormalization Group(DMRG)
+Density Matrix Renormalization Group is a powerful algorithm in studying a relatively large system. In modern physics, we are sometimes interested a system with large amount of particles(like 50-100 or so), which is impossible to calculate even for a computer
+
+However, through certain mathematical renormalization strategy, DMRG allows us to reduce the solution space we need to calculate, and calculate our most interested state(usually ground state) efficiently and precisely
+
+## It's like a brilliant editor for a quantum story, cutting out the fluff and keeping only the essential plot. ##
+
+
